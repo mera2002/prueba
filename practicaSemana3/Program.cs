dacedba70e73d002dc7fdd05f757b99e5ae4da1a
mera2002 <SMIS072121@ugb.edu.sv>
2021-08-11 20:28:53 -0600

    cambios

--- practicaSemana3/Program.cs
+++ practicaSemana3/Program.cs
@@ -10,6 +10,31 @@ namespace practicaSemana3
     {
         static void Main(string[] args)
         {
+            //loops
+            loopwhile();
+            Console.ReadKey();
         }
-    }
+
+        //while
+        //evalua la condicion y ejecuta las intrucciones 
+        //si la condicion es verdadera 
+
+        public static void loopwhile() {
+            int[] nums = new int[3]; //crear un arreglo 
+
+            //llenar arreglo 
+            nums[0] = 4;
+            nums[1] = 22;
+            nums[2] = 8;
+
+            //imprimir los numeros utilizando while 
+            int i = 0;
+            while(i < nums.Length)
+            {
+                Console.WriteLine("Indice {0}: {1}",i,nums[i]);
+                i++;
+            }
+
+        }
+}
 }
